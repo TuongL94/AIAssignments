792c71e51b73c2e297f652a7bbcd1a8371ab3392
ColinNordin <colin.nordin@gmail.com>
2017-02-03 12:31:03 +0100

    Add files via upload

--- Assignment1/Main.java
+++ Assignment1/Main.java
@@ -1,18 +1,50 @@
-import java.util.ArrayList;
-
 public class Main {
 	
     public static void main(String[] args) {
-    	State s1 = new State();
-    	State s2 = (State) s1.clone();
+    	State s = new State();
     	int identity = 1;
-    	Point myAction = new Point(3,2);
-    	s2.updateState(myAction, identity);
-    	int[][] matrix1 = s1.getMatrix();
-    	int[][] matrix2 = s2.getMatrix();
-    	System.out.println(matrix1[3][3]);
-    	System.out.println(matrix2[3][3]);
-    	
-
+    	Point myAction = new Point(2,3);
+		for(int i = 0; i <=7; i++){
+			for(int j = 0; j <= 7; j++){
+				if(s.getStateMatrix()[i][j] == -1) {
+					System.out.print("w"+"  ");
+				} else if(s.getStateMatrix()[i][j] == 1){
+					System.out.print("b"+"  ");
+				} else {
+					System.out.print(s.getStateMatrix()[i][j]+"  ");
+				}
+			}
+			System.out.print("\n");
+		}
+		System.out.print("\n");
+    	s.updateState(myAction, identity);
+    	int[][] matrix = s.getStateMatrix();
+		for(int i = 0; i <=7; i++){
+			for(int j = 0; j <= 7; j++){
+				if(s.getStateMatrix()[i][j] == -1) {
+					System.out.print("w"+"  ");
+				} else if(s.getStateMatrix()[i][j] == 1){
+					System.out.print("b"+"  ");
+				} else {
+					System.out.print(s.getStateMatrix()[i][j]+"  ");
+				}
+			}
+			System.out.print("\n");
+		}
+		System.out.print("\n");
+		Point a = new Point(2,2);
+		s.updateState(a, -identity);
+		for(int i = 0; i <=7; i++){
+			for(int j = 0; j <= 7; j++){
+				if(s.getStateMatrix()[i][j] == -1) {
+					System.out.print("w"+"  ");
+				} else if(s.getStateMatrix()[i][j] == 1){
+					System.out.print("b"+"  ");
+				} else {
+					System.out.print(s.getStateMatrix()[i][j]+"  ");
+				}
+			}
+			System.out.print("\n");
+		}
     }
 }
