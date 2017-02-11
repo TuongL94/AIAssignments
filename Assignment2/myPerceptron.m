86ea2fe07d540ee94b6d8d8b95e17a691071d1fe
ColinNordin <colin.nordin@gmail.com>
2017-02-11 15:19:57 +0100

    Add files via upload

---
+++ Assignment2/myPerceptron.m
@@ -0,0 +1,14 @@
+function [w] = myPerceptron(label, data)
+dataset = [ones(length(data),1) data];
+q = length(label);
+nbrOfMisclassified = q;
+alpha = 1;
+w = [0 0 0]';
+t = 0;
+while nbrOfMisclassified > 0
+    k = randi(q);
+    w = w + alpha*(label(k)-(dataset(k,:)*w >= 0))*dataset(k,:)';
+    nbrOfMisclassified = sum(((label-(dataset*w >= 0)) ~= 0));
+    t = t + 1;
+    alpha = 1000/(1000+t);
+end
