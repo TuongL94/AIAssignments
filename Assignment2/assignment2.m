3008d3bfe3ddb01ec5dc8ebe5d943d575a3fa306
ColinNordin <colin.nordin@gmail.com>
2017-02-16 12:29:44 +0100

    Add files via upload

--- Assignment2/assignment2.m
+++ Assignment2/assignment2.m
@@ -1,5 +1,6 @@
 %% Loading data
-
+% Enable the LIBSVM toolbox
+make
 % Load the data to matlab-compatible formats
 [label, dataset] = libsvmread('encodedData.txt');
 dataset = full(dataset);
@@ -44,7 +45,7 @@ w_stoch_fr(2) = max(frData(:,2))/max(frData(:,1))*scaled_w_stoch_fr(2);
 
 grid = linspace(10000,80000,1000);
 
-figure(1)
+figure(2)
 plot(engData(:,1),engData(:,2),'ro')
 hold on
 plot(grid, w_stoch_eng(1)+w_stoch_eng(2)*grid, 'r')
@@ -58,11 +59,12 @@ plot(grid, w_stoch_fr(1)+w_stoch_fr(2)*grid, 'b')
 m_scaled_percep = -w_scaled_percep(1)/w_scaled_percep(3);
 k_scaled_percep = -w_scaled_percep(2)/w_scaled_percep(3);
 
-m_percecp = max(dataset(:,2))*m_scaled;
-k_percep = max(dataset(:,2))/max(dataset(:,1))*k_scaled;
+m_percep = max(dataset(:,2))*m_scaled_percep;
+k_percep = max(dataset(:,2))/max(dataset(:,1))*k_scaled_percep;
 
 grid = linspace(10000,80000,70000);
 
+figure(3)
 plot(grid, k_percep*grid+m_percep, 'g')
 hold on
 plot(engData(:,1),engData(:,2),'ro')
@@ -75,11 +77,13 @@ plot(frData(:,1),frData(:,2),'bx')
 m_scaled_logistic = -w_scaled_logistic(1)/w_scaled_logistic(3);
 k_scaled_logistic = -w_scaled_logistic(2)/w_scaled_logistic(3);
 
-m_logistic = max(dataset(:,2))*m_scaled;
-k_logistic = max(dataset(:,2))/max(dataset(:,1))*k_scaled;
+m_logistic = max(dataset(:,2))*m_scaled_logistic;
+k_logistic = max(dataset(:,2))/max(dataset(:,1))*k_scaled_logistic;
 
 grid = linspace(10000,80000,70000);
 
+
+figure(4)
 plot(grid, k_logistic*grid+m_logistic, 'g')
 hold on
 plot(engData(:,1),engData(:,2),'ro')
