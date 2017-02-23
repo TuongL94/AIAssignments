a482d9b772f3f027a7ffc2ac2fda527e8a089661
ColinNordin <colin.nordin@gmail.com>
2017-02-23 16:23:45 +0100

    Add files via upload

--- Assignment3/DummyLocalizer.java
+++ Assignment3/DummyLocalizer.java
@@ -5,13 +5,13 @@ import control.EstimatorInterface;
 public class DummyLocalizer implements EstimatorInterface {
 		
 	private int rows, cols, head;
-	private int [][] transitionMatrix = new int [64][64];
+	private double [][] transitionMatrix = new double [64][64];
 
 	public DummyLocalizer( int rows, int cols, int head) {
 		this.rows = rows;
 		this.cols = cols;
 		this.head = head;
-		
+		//createTransitionMatrix();
 	}	
 	
 	public int getNumRows() {
@@ -27,7 +27,9 @@ public class DummyLocalizer implements EstimatorInterface {
 	}
 	
 	public double getTProb( int x, int y, int h, int nX, int nY, int nH) {
-		return 0.0;
+		int currentState = 4*(y+4*x)+h;
+		int newState = 4*(nY+4*nX)+nH;
+		return transitionMatrix[currentState][newState];
 	}
 
 	public double getOrXY( int rX, int rY, int x, int y) {
@@ -62,11 +64,48 @@ public class DummyLocalizer implements EstimatorInterface {
 	private void createTransitionMatrix() {
 		for(int i = 0; i < transitionMatrix.length; i++) {
 			for(int j = 0; j < transitionMatrix.length; j++) {
+				int[] row_coord = getCoord(i);
+				int[] col_coord = getCoord(j);
+				int row_heading = i % 4;
+				int col_heading = j % 4;
+				if(Math.abs(row_coord[0]-col_coord[0]) > 1 || Math.abs(row_coord[1]-col_coord[1]) > 1 ||
+						((row_coord[0] != col_coord[0]) &&  (row_coord[1] != col_coord[1]))){
+					transitionMatrix[i][j] = 0;
+				} else {
+					if((row_coord[0]-col_coord[0] < 0 && col_heading == 1) || (row_coord[0]-col_coord[0] > 0 && col_heading == 3)
+							|| (row_coord[1]-col_coord[1] < 0 && col_heading == 0) || (row_coord[1]-col_coord[1] > 0 && col_heading == 2)){
+						if(wall(row_coord[0],row_coord[1],row_heading) && isCorner(row_coord)) {
+							transitionMatrix[i][j] = 0.5;
+						} else if (wall(row_coord[0],row_coord[1],row_heading)){
+							transitionMatrix[i][j] = 1.0/3.0;
+						} else {
+							if(row_heading == col_heading){
+								transitionMatrix[i][j] = 0.7;
+							} else {
+								transitionMatrix[i][j] = 0.3/3.0;
+							}
+						}
+					} else {
+						transitionMatrix[i][j] = 0;
+					}
+				}
 				
 			}
 		}
 	}
 	
+
+	private boolean isCorner(int[] coord) {
+		return((coord[0]==0 && coord[1]==0) || (coord[0]==0 && coord[1]==3) || (coord[0]==3 && coord[1]==0) || (coord[0]==3 && coord[1]==3));
+	}
+
+	private int[] getCoord(int j) {
+		int[] coord = new int[2];
+		coord[1] = j%4;
+		coord[0] = (int) Math.floor(((float) j/4 - coord[1])/4);
+		return coord;
+	}
+
 	private boolean wall(int x, int y, int h) {
 		boolean foundWall = false;
 		switch(h) {
