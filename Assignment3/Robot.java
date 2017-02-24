add98ff2b9cb56ca6b5c2dc588f333218a05ffea
ColinNordin <colin.nordin@gmail.com>
2017-02-24 10:25:56 +0100

    Add files via upload

--- Assignment3/Robot.java
+++ Assignment3/Robot.java
@@ -1,14 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Robot {
 	
 	private int[] pos;
 	private int heading;
+	Random randomizer;
 	
 	public Robot(){
-		int row = (int) Math.floor(Math.random() * 4);
-		int col = (int) Math.floor(Math.random() * 4);
-		heading = (int) Math.floor(Math.random() * 4);
+		randomizer = new Random();
+		int row = randomizer.nextInt(4);
+		int col = randomizer.nextInt(4);
+		heading = randomizer.nextInt(4);
 		pos[0] = row;
 		pos[1] = col;
 	}
@@ -18,7 +23,34 @@ public class Robot {
 	}
 	
 	public void walk(){
+		if(Utilities.wall(pos[0], pos[1], heading)){
+				ArrayList<Integer> feasibleHeadings = Utilities.getFeasibleHeadings(pos);
+				heading = feasibleHeadings.get(randomizer.nextInt(feasibleHeadings.size()));
+				step();
+		} else {
+			if(randomizer.nextDouble() > 0.7){
+				ArrayList<Integer> feasibleHeadings = Utilities.getFeasibleHeadings(pos);
+				int newHeading = heading;
+				while(newHeading == heading){
+					newHeading = feasibleHeadings.get(randomizer.nextInt(feasibleHeadings.size()));
+				}
+				heading = newHeading;
+			}
+			step();
+		}
 		
+	}
+
+	private void step() {
+		if(heading == 0){
+			pos[0] = pos[0] - 1;
+		} else if(heading == 1){
+			pos[1] = pos[1] + 1;
+		} else if(heading == 2){
+			pos[0] = pos[0] + 1;
+		} else if(heading == 3){
+			pos[1] = pos[1] - 1;
+		}
 	}
 
 }
